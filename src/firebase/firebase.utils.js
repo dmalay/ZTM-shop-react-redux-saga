f380f1e39ce23a39f9a4fffe9073b18461dcf0f4
dmalay <dmalay1228@gmail.com>
2021-02-08 22:27:33 +0300

    signup and signin with storing users into FirestoreDB

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,9 +10,34 @@ const config = {
   messagingSenderId: "592394195064",
   appId: "1:592394195064:web:b4a11b8f5375af5a895d8d",
   measurementId: "G-T6BS30TXTY"
-};
+}
 
 firebase.initializeApp(config)
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef
+}
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
